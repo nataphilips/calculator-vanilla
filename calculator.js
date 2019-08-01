00a60c4b6c240984068719e6be2893c234860eba
Natalia Filippova <fnd.natalia@gmail.com>
2019-08-01 17:10:22 +1000

    Added addDigit method

--- calculator.js
+++ calculator.js
@@ -11,3 +11,25 @@ var state = Object.assign({}, initialState);
 function reset() {
   state = Object.assign({}, initialState);
 }
+
+function addDigit(x) {
+    var a = state.aState;
+    if (x == 0 && state.input == 0) {
+      a = "0";
+    } else {
+        if (a === "0" && x == ".") {
+          a = a.concat(x);
+        } else if (x == "." && a.indexOf(".") === -1) {
+          a = a.concat(x);
+        } else if (state.newCalculation === true) {
+          a = x.toString(10);
+        } else if (a == "0") {
+          a = x.toString(10);
+        } else {
+          a = a.concat(x.toString(10));
+        }
+      state.input = a;
+      state.aState = a;
+      state.newCalculation = false;
+    }
+  }
